18c63a202048810c61c84a49e9927e0d8cf501da
Aaron Na <syn228@nyu.edu>
2018-04-10 20:01:25 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
   squared_numbers = []
-  array.each do |numbers|squared_numbers<<
-    numbers**2
+  array.each do |numbers|squared_numbers<< numbers**2
+    numbers
   end
 end
