d7a20c625e8a200a6cccddb983535816dddf91d2
Aaron Na <syn228@nyu.edu>
2018-04-10 20:02:40 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
   squared_numbers = []
-  array.each{|numbers|squared_numbers<< numbers**2}
+  array.each {|numbers|squared_numbers<< numbers**2}
     squared_numbers
-  end
+
 end
